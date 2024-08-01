75b9fb2322abc998b6f1cd0f4a871c6627b5e480
beingyash <beingyash2712@gmail.com>
2024-08-01 23:33:41 +0530

    fetching successfull

--- app/seed/router.js
+++ app/seed/router.js
@@ -1,9 +1,10 @@
-import { db } from '@vercel/postgres';
+import { db } from "@vercel/postgres";
+import bcrypt from "bcrypt";
+import { users } from "../lib/placeholder-data";
 
 const client = await db.connect();
 
-export async function seedUsers() {
-//   console.log(client);
+export default async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
     CREATE TABLE IF NOT EXISTS users (
@@ -14,5 +15,17 @@ export async function seedUsers() {
     );
   `;
 
-}
 
+  const insertedUsers = await Promise.all(
+    users.map(async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      return client.sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+    })
+  );
+
+  return insertedUsers;
+}
